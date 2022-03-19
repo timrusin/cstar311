962597359f6f2261fb303ef420f1f9a267747add
timrusin <timrusin@gmail.com>
2022-03-19 16:33:13 -0500

    added logo working on nav

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import { useState, React } from 'react'
 import { Route, Routes, Link } from 'react-router-dom'
 import './App.css';
+import logo from "./assets/photos/cstar311logo.png"
 
 function App() {
+
   return (
-      <div className="App">
-        <nav>
-          <Link to="/" className="button">Home</Link>
-          <Link to="/about" className="button">About</Link>
-          <Link to="/photos" className="button">Photos</Link>
-          <Link to="/contact" className="button">Contact</Link>
-        </nav>
-        <Routes>
-          <Route></Route>
-          <Route></Route>
-          <Route></Route>
-        </Routes>
-      </div>
+    <Navbar></Navbar>
+  );
+}
+
+function Navbar() {
+  const [open, setOpen ]= useState(false)
+
+  return (
+    <div className="navbar">
+      <nav className="navbar-nav">
+        <Link to="/photos" className="button">Images</Link>
+        <Link to="/">
+          <img className="logo" src={logo} alt="logo"></img>
+        </Link>
+        <Link to="/about" className="button">About</Link>
+      </nav>
+      <Routes>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+      </Routes>
+    </div>
   );
 }
 
