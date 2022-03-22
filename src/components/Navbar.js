a492c015a6ff20ffed360aa461180358d813a7fa
timrusin <timrusin@gmail.com>
2022-03-22 16:24:36 -0500

    working on dropdown for images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import { React, useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../assets/photos/cstar311logo.png'
+import Dropdown from './Dropdown'
 
 const Navbar = () => {
+    const [click, setClick]=useState(false)
+    const [dropdown, setDropdown]=useState(false)
+
+    const handleClick = () => setClick(!click)
+    
   return (
     <nav className="navbar">
       <Link to="/images" className="nav-item">
         Images
       </Link>
+      {dropdown && <Dropdown/>}
       <Link to="/">
         <img className="logo" src={logo} alt="logo"></img>
       </Link>
       <Link to="/about" className="nav-item">
         About
       </Link>
+      <div className='menu-icon' onClick={handleClick}>
+        <i className= {click ? 'fas fa-times' : 'fas fa-bars'}/>
+      </div>
+      <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          
+      </ul>
     </nav>
   );
 }
