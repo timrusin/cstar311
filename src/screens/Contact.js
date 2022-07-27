bb9685c8111a4260242d6eea135dfae189e169f1
timrusin <timrusin@gmail.com>
2022-07-26 21:00:13 -0500

    added netlify redirects file

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './Contact.css'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <form className='contact-form'>
+      <label>
+        Name: 
+        <input type="text" name="name" className='form-input' />
+      </label>
+      <label>
+        Email:
+        <input type="text" name="email" className='form-input'/>
+      </label>
+      <label>
+        Message:
+        <textarea></textarea>
+      </label>
+      </form>
   )
 }
 
